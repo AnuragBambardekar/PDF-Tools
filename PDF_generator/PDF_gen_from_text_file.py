020c7a78dff91cc4a9f67aa10d3a6ba53e77e0b5
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-08-08 14:12:41 -0400

    generating PDF files from text

--- PDF_generator/PDF_gen_from_text_file.py
+++ PDF_generator/PDF_gen_from_text_file.py
@@ -1,20 +1,14 @@
 from fpdf import FPDF
   
-# save FPDF() class into a variable pdf
 pdf = FPDF()  
-  
-# Add a page
+
 pdf.add_page()
-  
-# set style and size of font that you want in the pdf
+
 pdf.set_font("Arial", size = 12)
- 
-# open the text file in read mode
+
 f = open("./input.txt", "r")
- 
-# insert the texts in pdf using MultiCell
+
 for x in f:
     pdf.multi_cell(0, 5, txt = x) # width, height, text_content
-  
-# save the pdf with name mygfg.pdf
+
 pdf.output("gen_PDF_from_text.pdf")  
